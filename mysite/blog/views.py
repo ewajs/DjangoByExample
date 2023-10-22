27918ccb0e5e8632f3afe73857426a34a877c855
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2023-10-22 19:19:45 -0300

    Share template and send (fake) email

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -26,21 +26,28 @@ def post_detail(request, year, month, day, post):
     return render(request,'blog/post/detail.html', {'post': post})
 
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
     if request.method == 'POST':
         # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Form fields passed validation
             cd = form.cleaned_data
-            # ... send email
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n{cd['name']}'s comments: {cd['comments']}"
+            send_mail(subject, message, 'sender@gmail.com', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
     
-    return render(request, 'blog/post/share.html', {'post': post,'form': form})
+    return render(request, 'blog/post/share.html', {'post': post,'form': form, 'sent': sent})
 
 
 # Class based view example of Posts List
