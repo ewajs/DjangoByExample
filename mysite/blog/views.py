7f9fac5fa63d0f27dc77e3d4125488f7f638914d
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2023-10-22 18:30:42 -0300

    Class based views post list

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -24,3 +24,14 @@ def post_detail(request, year, month, day, post):
                              publish__month = month,
                              publish__day = day)
     return render(request,'blog/post/detail.html', {'post': post})
+
+
+from django.views.generic import ListView
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
