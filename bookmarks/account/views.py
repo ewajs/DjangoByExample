d8ea62328c28bce4bdfccf72edae8b80444b688f
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2023-10-26 18:54:12 -0300

    add dashboard and log in out views

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from .forms import LoginForm
 
+# Unused in favor of default django auth view
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
@@ -21,3 +23,9 @@ def user_login(request):
         form = LoginForm()
 
     return render(request, 'account/login.html', {'form': form})
+
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
