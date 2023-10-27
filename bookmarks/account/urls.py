f16a0f5d6f6ec6f82a2623374528b8048f870ac5
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2023-10-27 12:17:51 -0300

    pw change views

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -4,7 +4,12 @@ from . import views
 
 urlpatterns = [
     #path('login/', views.user_login, name='login'),
+    # Log in-out
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # PW Change
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    
     path('', views.dashboard, name='dashboard'),
 ]
