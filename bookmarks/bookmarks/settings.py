b2f57c9080a75625aa4edf7adec8de0b2107b332
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2023-11-18 16:59:55 -0300

    Add signal and field to denormalize and update image likes in a separate field

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -56,9 +56,11 @@ INSTALLED_APPS = [
     "images.apps.ImagesConfig",
     "easy_thumbnails",
     "actions.apps.ActionsConfig",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
