7fc727b99fa3c6077de5d34cc1d73209dee95ccf
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2023-12-14 18:40:39 -0300

    Added coupons app, models and integration with cart

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -1,7 +1,9 @@
 from decimal import Decimal
-from django.conf import settings
-from shop.models import Product
 
+from django.conf import settings
+
+from shop.models import Product
+from coupons.models import Coupon
 class Cart:
     def __init__(self, request):
         """
@@ -15,6 +17,8 @@ class Cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         
         self.cart = cart
+        # store current applied coupon
+        self.coupon_id = self.session.get('coupon_id')
 
     def __iter__(self):
         """
@@ -37,6 +41,15 @@ class Cart:
         Count all items in the cart.
         """
         return sum(item['quantity'] for item in self.cart.values())
+    
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DoesNotExist:
+                pass
+            return None
     
     def save(self):
         # mark the session as "modified" to make sure it gets saved
@@ -73,3 +86,11 @@ class Cart:
 
     def get_total_price(self):
         return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+    
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount / Decimal(100)) * self.get_total_price()
+        return Decimal(0)
+    
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
+
 from shop.models import Product
+from coupons.forms import CouponApplyForm
 from .cart import Cart
 from .forms import CartAddProductForm
+
 
 @require_POST
 def cart_add(request, product_id):
@@ -27,4 +30,6 @@ def cart_detail(request):
     cart = Cart(request)
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'override': True})
-    return render(request, 'cart/detail.html', {'cart': cart})
+    
+    coupon_apply_form = CouponApplyForm()
+    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form})
