58cd41ef9c9b143139707252b7eae48982cb99a0
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2023-12-14 18:52:12 -0300

    Added messages for coupons found/not found

--- myshop/coupons/views.py
+++ myshop/coupons/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.utils import timezone
 from django.views.decorators.http import require_POST
+from django.contrib import messages
 
 from .models import Coupon
 from .forms import CouponApplyForm
@@ -14,7 +15,9 @@ def coupon_apply(request):
         try:
             coupon = Coupon.objects.get(code__iexact=code, valid_from__lte=now, valid_to__gte=now, active=True)
             request.session['coupon_id'] = coupon.id
+            messages.success(request, 'Coupon applied succesfully!')
         except Coupon.DoesNotExist:
             request.session['coupon_id'] = None
+            messages.error(request, 'Coupon not found!')
     
     return redirect('cart:cart_detail')
