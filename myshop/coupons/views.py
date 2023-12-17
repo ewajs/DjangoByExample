9d37cef431dcafb177d8178167e1f25fb1eb32c1
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2023-12-17 12:12:52 -0300

    Lots of other translations

--- myshop/coupons/views.py
+++ myshop/coupons/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.utils import timezone
 from django.views.decorators.http import require_POST
 from django.contrib import messages
+from django.utils.translation import gettext_lazy as _
 
 from .models import Coupon
 from .forms import CouponApplyForm
@@ -15,9 +16,9 @@ def coupon_apply(request):
         try:
             coupon = Coupon.objects.get(code__iexact=code, valid_from__lte=now, valid_to__gte=now, active=True)
             request.session['coupon_id'] = coupon.id
-            messages.success(request, 'Coupon applied succesfully!')
+            messages.success(request, _('Coupon applied succesfully!'))
         except Coupon.DoesNotExist:
             request.session['coupon_id'] = None
-            messages.error(request, 'Coupon not found!')
+            messages.error(request, _('Coupon not found!'))
     
     return redirect('cart:cart_detail')
