4b6bc215cd9266358b5d8cd22b07edc571d15abd
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2023-12-30 18:54:14 -0300

    Add Django Debug Toolbar

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "students.apps.StudentsConfig",
     "embed_video",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -140,3 +142,6 @@ CACHES = {
         'LOCATION': '127.0.0.1:11211',
     }
 }
+
+# Debug Toolbar Allowed IPs
+INTERNAL_IPS = ['127.0.0.1']
