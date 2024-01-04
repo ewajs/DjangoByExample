309a361bbe154388ced89a5c767d1a710c3a9f32
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2024-01-04 16:51:00 -0300

    Add Authenticated and Permissioned API View to enroll users to courses via POST

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,6 +1,12 @@
-from rest_framework import generics
+from django.shortcuts import get_object_or_404
 
-from courses.models import Subject
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from courses.models import Subject, Course
 from courses.api.serializers import SubjectSerializer
 
 class SubjectListView(generics.ListAPIView):
@@ -10,3 +16,12 @@ class SubjectListView(generics.ListAPIView):
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+class CourseEnrollView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
